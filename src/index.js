6e62113c14061b21d4b630e3dd255734a3ffb7bd
tomsonst <40933547+tomsonst@users.noreply.github.com>
2020-10-06 20:23:31 +0300

    feat: add solution of task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, arr) {
+  let arrTest =[];
+  let k =0;
+  for(let i =0;i<arr.length; i++){
+    let strTest = arr[i][0] +arr[i][1];
+    arrTest.push(strTest);
+  }
+  for(let c = 0; c< 50; c++){
+    
+    for(let i =0; i<arrTest.length;i++){
+   
+      str = str.replace(arrTest[i], '');
+      
+    }
+    
+  }
+  
+  return(str.length === 0 ? true: false);
 }
